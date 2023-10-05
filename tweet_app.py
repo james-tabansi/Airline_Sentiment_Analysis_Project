344e12e3a59168a73ad766a4856e8c0fd45b8de7
James Tabansi <jamesdivine998@gmail.com>
2023-10-05 05:44:31 +0100

    streamlit app file added

---
+++ tweet_app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+import pandas as pd
+import numpy as np
+import pickle
+import sklearn
+
+ 
+#load the trained model
+model = tf.keras.models.load_model('/Users/user/Documents/MLOps/airline_tweet_project/simplernn_model.h5')
+
+#load the encoder
+enc_path = '/Users/user/Documents/MLOps/airline_tweet_project/needed_files/enc.pkl'
+with open(enc_path, 'rb') as file:
+    encoder = pickle.load(file)
+
+#load the vectorizer
+load_path = '/Users/user/Documents/MLOps/airline_tweet_project/tokenizer.pkl'
+with open(load_path, mode='rb') as f:
+    tk = pickle.load(f)
+
+
+
+#pipeline for preprocessing
+def input_preprocessing(text):
+    #tokenize the text
+    token = tk.texts_to_sequences([text])
+
+    #pad the tokens
+    token = pad_sequences(token, maxlen=28)
+    return token[0]
+
+#create app Title
+st.title("Tweet Classification App")
+
+#Welcome user
+st.write('Welcome to our Airline Sentiment Analysis Application. Kindly give us the texts you need us to analyze')
+
+#create user input interface
+user_input = st.text_area(f"Enter Text Here", " ")
+#st.write(user_input)
+
+#create a button for prediction
+button = st.button(f"Predict Sentiment")
+
+if button and user_input is not None:
+    #first preprocess text
+    prep_text = input_preprocessing(user_input)
+
+    prep_text = np.array(prep_text)  # Assuming prep_text is a list or array of text sequences
+    prep_text = prep_text.reshape(-1, 28)
+
+    #make prediction
+    pred = model.predict(prep_text)
+
+
+    #get the sentiment label
+    sentiment = encoder.inverse_transform(pred)
+
+
+    st.write(f'The Sentiment of the given text is: {sentiment}')
+
+
